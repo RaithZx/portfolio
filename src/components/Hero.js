e678dd557376f3ede02c9569411a61150b845fcf
Yuri Leandro <y.leandrof@gmail.com>
2021-02-13 10:51:09 -0100

    Allowed connections from devices on LAN

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div className="flex flex-col flex-wrap justify-center h-screen content-center max-w-7xl">
-            <h1 className="text-6xl">Hi! I'm Yuri 👋🏽<br/> a Salesforce developer</h1>
-            <p className="mt-8 text-2lg text-gray-600">Currently working remotely from Africa.<br/>
-Read more about me here or checkout my previous projects</p>
-            <span className="" role="img">
+        <div className="flex flex-col flex-wrap justify-center content-center w-2/3 h-screen">
+            <h1 className="text-6xl text-indigo-900">Hi! I'm Yuri 👋🏽<br/> a Salesforce developer</h1>
+            <span className="mt-8 text-xl text-gray-600">Currently working remotely from Africa.<br/></span>
+<span className="text-gray-600 text-xl">Read more <a href="" className="text-black-300">about me here</a> or checkout my <a>previous projects</a></span>
+            <span className="mt-9" role="img">
                 Arrow
                 {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="50px" height="50px" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M5 11l7-7 7 7M5 19l7-7 7 7" />
