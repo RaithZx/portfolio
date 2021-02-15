e75f9418cebcd2638acc461cc24a4bee17297352
Yuri Leandro <y.leandrof@gmail.com>
2021-02-15 14:55:05 -0100

    nothing changed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div className="flex flex-col flex-wrap justify-center content-center w-2/3 h-screen">
-            <h1 className="text-6xl text-indigo-900">Hi! I'm Yuri 👋🏽<br/> a Salesforce developer</h1>
-            <span className="mt-8 text-xl text-gray-600">Currently working remotely from Africa.<br/></span>
-<span className="text-gray-600 text-xl">Read more <a href="" className="text-black-300">about me here</a> or checkout my <a>previous projects</a></span>
+        <div className="flex flex-col flex-wrap justify-center content-center  w-2/3 md:w-full h-screen">
+            <h1 className="text-6xl text-indigo-900">Hi! I'm Yuri 👋🏽<br /> a Salesforce developer</h1>
+            <span className="mt-8 text-xl text-gray-600">Currently working remotely from Africa.<br /></span>
+            <span className="text-gray-600 text-xl">Read more <a href="" className="text-black-300">about me here</a> or checkout my <a>previous projects</a></span>
             <span className="mt-9" role="img">
                 Arrow
                 {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="50px" height="50px" viewBox="0 0 24 24" stroke="currentColor">
