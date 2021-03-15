92686d4a5a2a8e36a6b6708b287ed647a837f763
Yuri Lendro <y.lendrof@gmail.com>
2021-03-15 17:27:31 -0100

    adding pages

---
+++ src/pages/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Layout from "../components/Layout";
+const About = () => {
+  return (
+    <Layout>
+      <section className="flex flex-col flex-wrap mt-10 h-screen" id="about">
+        <a href="">
+          <div class="flex items-center justify-start mb-8">
+            <div class="w-10 border-b border-solid border-black"></div>
+            <h1 class="ml-3 text-4xl text-primary title">About me</h1>
+            {/* <h1 class="ml-3 text-3xl text-gray-700 font-bold title">Work Experience</h1> */}
+          </div>
+        </a>
+        <div class="text-xl text-secondary mb-10">
+          My name is Yuri and I'm 21 years old. I started coding when I was 12
+          and fell in love with web technologies. <br />
+          I'm an avid web developer based in Spain. I love to learn and explore
+          so I ask a lot of questions :P <br />
+          I always look forward to improve and adopt best practices in my
+          workflow. I have an strong analytical mindset that helps me provide a
+          problem solving asset. <br />I have +4 years of experience working for
+          large and small companies.
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export default About;
---
+++ src/pages/Hero.js
@@ -0,0 +1,48 @@
+import React from "react";
+import profilePic from "../images/profile.jpg";
+import profilePicHat from "../images/profileHat.jpg";
+import Card from "../components/Card";
+import Media from "../components/SocialMedia";
+import Layout from "../components/Layout";
+import { Link } from "gatsby";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col flex-wrap justify-center  h-screen relative">
+      <h1 className="text-5xl md:text-6xl text-gray-700 font-bold title">
+        Hi! I'm Yuri 👋🏽,
+        <br /> a <span className="underline">Salesforce</span> developer
+      </h1>
+      <span className="mt-4 md:mt-8 text-xl text-secondary">
+        I'm an avid developer with +4 years of{" "}
+        <Link to="/WorkExperience">
+          <span className="underline">
+            <mark>work experience</mark>
+          </span>
+        </Link>
+        <br />
+      </span>
+      {/* <span className="mt-8 text-xl text-secondary">
+        I'm currently working <mark>remotely from Africa (Cape Verde)</mark> as
+        a digital nomad
+        <br />
+      </span> */}
+      <span className="text-gray-600 text-xl">
+        Read my{" "}
+        <mark>
+          <Link to="/About" className="underline text-black-300">
+            story
+          </Link>{" "}
+        </mark>
+        and checkout my{" "}
+        <a href="" className="underline text-black-300">
+          <mark> projects</mark>
+        </a>
+      </span>
+      <div className="flex justify-start">{/* <Card /> */}</div>
+      <Media />
+    </div>
+  );
+};
+
+export default Hero;
